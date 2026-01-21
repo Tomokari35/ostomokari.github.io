0da8064ad8e38b36af4bab8b707818f7cfc02140
Dev_Tomokari <217519694+Tomokari35@users.noreply.github.com>
2026-01-21 09:36:20 +0100

    Create portfolio.js

---
+++ portfolio.js
@@ -0,0 +1,21 @@
+<script>
+const buttons = document.querySelectorAll('[data-filter]');
+const cards = document.querySelectorAll('.portfolio');
+
+buttons.forEach(btn => {
+  btn.addEventListener('click', () => {
+    const filter = btn.dataset.filter;
+
+    cards.forEach(card => {
+      if (filter === 'all') {
+        // Si "Tout" → on cache tout
+        card.style.display = 'none';
+      } else {
+        // Sinon on affiche seulement la catégorie cliquée
+        card.style.display =
+          card.dataset.category === filter ? 'block' : 'none';
+      }
+    });
+  });
+});
+</script>
